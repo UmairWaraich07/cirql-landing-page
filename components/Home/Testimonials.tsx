072d5f9b7e4ad3e1de8dcbacbddfb2ac0a99ac78
Umair Amjad <umairwaraich2019@gmail.com>
2024-10-03 19:34:46 +0500

    add the testimonial of donald fenton

--- components/Home/Testimonials.tsx
+++ components/Home/Testimonials.tsx
@@ -6,23 +6,27 @@ const Testimonials = () => {
     <section className="bg-primary-light  ">
       <div className="py-10 md:py-24 max-w-[1440px] mx-auto px-5 sm:px-10 lg:px-16">
         <div className="flex items-center flex-col justify-center text-center">
-          <h2 className="text-dark text-4xl sm:text-5xl font-medium">
-            I took one meeting and it paid for the entire year!
+          <h2 className="text-dark text-2xl sm:text-3xl font-medium">
+            I get so many emails - the categorization really helps me, and now
+            all those spam emails are gone! Plus, I had someone pay for a
+            meeting with me and that covered the cost of this for the whole year
+            already.
           </h2>
 
           <div className="flex items-center justify-center flex-col mt-10">
             <Image
-              src="/images/avatar.png"
+              src="/images/donald-fenton.jpeg"
               alt="avatar"
               width={64}
               height={64}
               priority
               quality={100}
+              className="rounded-full"
             />
             <h4 className=" mt-4 font-semibold text-dark text-lg">
-              Maisie Tucker
+              Donald Fenton
             </h4>
-            <p className="text-base ">Marketing Director, US Company</p>
+            <p className="text-base ">GM, Industrial Manufacturing Company</p>
           </div>
         </div>
       </div>
